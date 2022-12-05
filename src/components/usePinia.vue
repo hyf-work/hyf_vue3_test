848255940b7833d14db661c8fd1b003ddc98af72
hyf-work <446999073@qq.com>
2022-12-05 14:01:58 +0800

    1

---
+++ src/components/usePinia.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue';
+import { useUserStore } from '@/store/user';
+const userStore  = useUserStore();
+ userStore.updateName('李四')
+const msg = ref(1)
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/login',
+        name: "Login",
+        meta: {
+            title: "登录"
+        },
+        component: () => import('@/pages/login.vue')
+    }
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+export default router
